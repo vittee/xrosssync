86f1cada73afc98a9e0a16957ccdb7d62a5c7a62
Wittawas Nakkasem <vittee@hotmail.com>
2026-05-08 22:39:37 +0700

    refactor(firmware): reset label color depth to rgb565_2Byte

--- firmware/src/ui/widgets/Label.h
+++ firmware/src/ui/widgets/Label.h
@@ -14,14 +14,14 @@ public:
     Label(
         int16_t x, int16_t y, const String& text,
         const lgfx::IFont* font = nullptr,
-        lgfx::color_depth_t colorDepth = lgfx::color_depth_t::rgb888_3Byte
+        lgfx::color_depth_t colorDepth = lgfx::color_depth_t::rgb565_2Byte
     );
 
     Label(
         int16_t x, int16_t y, int16_t w, int16_t h,
         const String& text = "",
         const lgfx::IFont* font = nullptr,
-        lgfx::color_depth_t colorDepth = lgfx::color_depth_t::rgb888_3Byte
+        lgfx::color_depth_t colorDepth = lgfx::color_depth_t::rgb565_2Byte
     );
 
     void setText(const String& text);
