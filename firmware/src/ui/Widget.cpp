950c97ebd4aad6845c4e510ab1ac7e645cef48f7
Wittawas Nakkasem <vittee@hotmail.com>
2026-05-08 11:26:47 +0700

    refactor(firmware): add `colorDepth` parameter

--- firmware/src/ui/Widget.cpp
+++ firmware/src/ui/Widget.cpp
@@ -1,11 +1,16 @@
 #include "Widget.h"
+#include <esp_log.h>
 
 namespace ui {
 
-Widget::Widget(int16_t x, int16_t y, int16_t w, int16_t h)
+Widget::Widget(int16_t x, int16_t y, int16_t w, int16_t h, lgfx::color_depth_t colorDepth)
     : BaseWidget(x, y, w, h)
 {
-    m_sprite.createSprite(w, h);
+    m_sprite.setColorDepth(colorDepth);
+
+    if (!m_sprite.createSprite(w, h)) {
+        ESP_LOGE("Widget", "createSprite failed (%dx%d, depth=%d)", w, h, (int)colorDepth);
+    }
 }
 
 void Widget::draw(LovyanGFX& dst, int16_t offsetX, int16_t offsetY) {
--- firmware/src/ui/Widget.h
+++ firmware/src/ui/Widget.h
@@ -8,7 +8,8 @@ using namespace lgfx;
 
 class Widget : public BaseWidget {
 public:
-    Widget(int16_t x, int16_t y, int16_t w, int16_t h);
+    Widget(int16_t x, int16_t y, int16_t w, int16_t h,
+           lgfx::color_depth_t colorDepth = lgfx::color_depth_t::rgb565_2Byte);
 
     void draw(LovyanGFX& dst, int16_t offsetX = 0, int16_t offsetY = 0) override;
 
